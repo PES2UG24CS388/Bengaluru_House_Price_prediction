5595911f1b5bb272ee37b430094c97d59eb055e4
R.Pooja <flowerblossom2k06@gmail.com>
2026-06-25 17:24:42 +0530

    Refactor loading artifacts and model prediction

--- server/util.py
+++ server/util.py
@@ -6,36 +6,37 @@ __locations = None
 __data_columns = None
 __model = None
 
-def get_estimated_price(location,sqft,bhk,bath):
+def get_estimated_price(location, sqft, bhk, bath):
     try:
-        loc_index=__data_columns.index(location.lower())
+        loc_index = __data_columns.index(location.lower())
     except:
-        loc_index=-1
+        loc_index = -1
 
-    x=np.zeros(len(__data_columns))
-    x[0]=sqft
-    x[1]=bath
-    x[2]=bhk
-    if loc_index>=0:
-        x[loc_index]=1
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
 
-    return round(__model.predict([x])[0],2)
+    if loc_index >= 0:
+        x[loc_index] = 1
+
+    return round(__model.predict([x])[0], 2)
 
 def get_location_names():
     return __locations
 
 def load_saved_artifacts():
-    print("loading saved artifacts...start")
+    print("Loading saved artifacts...start")
 
     global __data_columns
     global __locations
     global __model
 
-    with open("./artifacts/columns.json","r") as f:
-        __data_columns = json.load(f)['data_columns']
+    with open("./artifacts/columns.json", "r") as f:
+        __data_columns = json.load(f)["data_columns"]
         __locations = __data_columns[3:]
 
-    with open("./artifacts/banglore_home_prices_model.pickle","rb") as f:
+    with open("./artifacts/banglore_home_prices_model.pickle", "rb") as f:
         __model = pickle.load(f)
 
-    print("Loading Completed...")
+    print("Loading saved artifacts...done")
